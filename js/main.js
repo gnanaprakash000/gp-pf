0903acb56c10fdb2d98bafe204dc981e87ee8535
gnanaprakash000 <146754432+gnanaprakash000@users.noreply.github.com>
2024-02-27 19:36:10 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+//show menu
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+
+showMenu("nav_toggle", "nav_menu");
+
+// active & remove active
+const navLink = document.querySelectorAll(".nav_link");
+navLink.forEach((n) => n.classList.remove("active"));
+
+function linkAction() {
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
+  const navMenu = document.getElementById("nav_menu");
+  navMenu.classList.remove("show");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// contact form
+
+const form = document.querySelector("form");
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "username",
+    Password: "password",
+    To: "them@website.com",
+    From: "you@isp.com",
+    Subject: "This is the subject",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+}
